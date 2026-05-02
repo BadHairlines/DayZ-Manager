a053ead7cc45eced9d7440ace9637e641d75761e
BadHairlines <105704515+BadHairlines@users.noreply.github.com>
2026-05-02 00:05:43 -0400

    Fix: Add missing import of ensure_connection in __init__.py

--- cogs/__init__.py
+++ cogs/__init__.py
@@ -1,4 +1,7 @@
 # Back-compat: allow main.py to call utils.init_db()
+from cogs.utils import ensure_connection
+
+
 async def init_db():
     """Initialize the DB pool (alias to ensure_connection)."""
     await ensure_connection()
