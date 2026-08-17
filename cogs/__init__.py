7bab209edf328a3a0c12fd30ffc7aa8151c44546
BadHairlines <105704515+BadHairlines@users.noreply.github.com>
2026-08-17 02:27:20 -0400

    Remove init_db function and back-compat code
    Removed the init_db function and its back-compatibility.

--- cogs/__init__.py
+++ cogs/__init__.py
@@ -1,7 +1,1 @@
-# Back-compat: allow main.py to call utils.init_db()
-from cogs.utils import ensure_connection
 
-
-async def init_db():
-    """Initialize the DB pool (alias to ensure_connection)."""
-    await ensure_connection()
